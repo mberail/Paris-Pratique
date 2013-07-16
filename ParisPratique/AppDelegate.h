ca1f79a667bec3d889390025f311ecb088c1fa3b
Maxime Berail <mberail@Maxs-Pro.local>
2013-07-16 02:52:46 +0200

    Initial Commit

---
+++ ParisPratique/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ParisPratique
+//
+//  Created by Maxime Berail on 16/07/13.
+//  Copyright (c) 2013 Maxime Berail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ParisPratique/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParisPratique
+//
+//  Created by Maxime Berail on 16/07/13.
+//  Copyright (c) 2013 Maxime Berail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
